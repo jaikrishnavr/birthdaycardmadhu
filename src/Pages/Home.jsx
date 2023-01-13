602e7fad908d45fec89df59f6cbc02298695e64f
jaikrishnavr <jaikrishnagokul@gmail.com>
2023-01-13 13:45:44 +0530

    first birthday commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css'
+
+const home = () => {
+  return (
+    <div><h1 class="text">card</h1></div>
+  )
+}
+
+export default home;
---
+++ src/Pages/Loadingscreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Loadingscreen.css'
+
+const Loadingscreen = () => {
+  return (
+    <div class="center">
+         <div class="ring" id="demo"></div>
+         <span>Greeting card loading...</span>
+      </div>
+  )
+}
+
+export default Loadingscreen
